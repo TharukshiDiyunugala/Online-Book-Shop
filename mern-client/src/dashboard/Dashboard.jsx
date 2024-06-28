fb2803f511738d22b2016ba254c619326c1ba3da
Tharukshi Diyunugala <139604971+TharukshiDiyunugala@users.noreply.github.com>
2024-06-28 17:15:05 +0530

    test changes

--- mern-client/src/dashboard/Dashboard.jsx
+++ mern-client/src/dashboard/Dashboard.jsx
@@ -23,6 +23,11 @@ const Dashboard = () => {
                 {book.bookTitle}
               </p>
             </h5>
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              <p>
+              Here are  acquisitions of 2021 so far, in reverse chronological order.
+              </p>
+            </h5>
             <p className="font-normal text-gray-700 dark:text-gray-400">
               <p>
                 Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
